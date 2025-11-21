96379583c4534038eea92af0bd392153b2c09c72
Ved Badadhe <vbbadadhe@gmail.com>
2025-11-21 13:37:11 +0530

    Remove .next from repo

--- nextjs/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/ved/attack-capital/nextjs/app/layout\":[\"static/media/4cf2300e9c8272f7.p.woff2\",\"static/media/93f479601ee12b01.p.woff2\"]},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
